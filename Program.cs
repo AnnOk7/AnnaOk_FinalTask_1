976ba6f152ef7e9fc4c3c9fa0145d2c3c791518b
Anna <ann.ok797@gmail.com>
2023-03-11 22:41:31 +0300

    2 commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. При решение не рекомендуется пользоваться 
+// коллекциями, лучше обойтись исключительно массивами
+
+Console.Clear();
+
+string[] array1 = new string[4] {"1234", "2567", "-2", "computer science"};
+string[] array2 = new string[array1.Length];
+
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
+void PrintArray(string[] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+        {
+            Console.Write($"{inArray[i]}\t ");
+        }
+        Console.WriteLine();
+    
+}
+SecondArray(array1, array2);
+PrintArray(array2);
